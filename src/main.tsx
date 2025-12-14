a88f5c992912f08de34d35119d2d77975be7d53a
farukkandemir <farukkandemir09@gmail.com>
2025-12-14 11:01:33 -0500

    refactor Toaster component import and update its properties for enhanced notification display in main app

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
-import { Toaster } from "sonner";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <Toaster />
+        <Toaster position="top-center" duration={3000} richColors />
         <App />
       </ThemeProvider>
     </BrowserRouter>
