ce04c6f2f21a414b17a3dff0ec68776c9fe88f91
farukkandemir <farukkandemir09@gmail.com>
2025-12-18 22:58:16 -0500

    some layout changes

--- src/routes/_authenticated/library.tsx
+++ src/routes/_authenticated/library.tsx
@@ -58,7 +58,7 @@ function LibraryPage() {
               Upload
             </Button>
           </div>
-          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {songs.map((song) => (
               <SongCard key={song.id} song={song} />
             ))}
